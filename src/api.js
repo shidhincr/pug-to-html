d240619790b498007aa99c390b39ca4660e4b05c
Shidhin <shidhincr@gmail.com>
2024-07-24 15:36:20 +0530

    feat: add comments

--- src/api.js
+++ src/api.js
@@ -1,17 +1,19 @@
-import lex from 'pug-lexer';
-import parse from 'pug-parser';
-import wrap from 'pug-runtime/wrap';
-import generateCode from 'pug-code-gen';
+import lex from "pug-lexer";
+import parse from "pug-parser";
+import wrap from "pug-runtime/wrap";
+import generateCode from "pug-code-gen";
 
-export function compilePugTemplate(str = '', locals = {}) {
+// this is a comment
+
+export function compilePugTemplate(str = "", locals = {}) {
   try {
     let funcStr = generateCode(parse(lex(str)), {
       compileDebug: false,
       pretty: true,
       inlineRuntimeFunctions: false,
-      templateName: '_parse'
+      templateName: "_parse",
     });
-    let func = wrap(funcStr, '_parse');
+    let func = wrap(funcStr, "_parse");
     return func(locals);
   } catch (e) {
     return `Compile Error: ${e.message}`;
